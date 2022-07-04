a2bdff979043b45e13afe233b6d3a45ac47e4746
Bagas12345678 <106974492+Bagas12345678@users.noreply.github.com>
2022-07-04 10:00:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,15 +19,15 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is Bagaszbot🔥
+My Creator is Bagaszoffc😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+62881010244718'] //Owner number in aive msg
+global.premium = ['+62881010244718'] //Owner Number info
+global.ownernomer = '+62881010244718' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = 'BagasOffc' //Owner Name
+global.botname = ' BagasBoT' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
 global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
